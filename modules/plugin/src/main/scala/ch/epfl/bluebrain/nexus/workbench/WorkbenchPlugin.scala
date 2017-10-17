c2dad5ca7c7e0772957cb3fa8d61406e421a184c
Bogdan Roman <bogdanromanx@gmail.com>
2017-10-17 16:24:15 +0200

    Ensure plugin is loaded after JvmPlugin (#3)
    The JvmPlugin resets the values of the sourceGenerators.

--- modules/plugin/src/main/scala/ch/epfl/bluebrain/nexus/workbench/WorkbenchPlugin.scala
+++ modules/plugin/src/main/scala/ch/epfl/bluebrain/nexus/workbench/WorkbenchPlugin.scala
@@ -2,10 +2,11 @@ package ch.epfl.bluebrain.nexus.workbench
 
 import sbt.Keys._
 import sbt._
+import sbt.plugins.JvmPlugin
 
 object WorkbenchPlugin extends AutoPlugin {
 
-  override lazy val requires = empty
+  override lazy val requires = JvmPlugin
   override lazy val trigger  = noTrigger
 
   trait Keys {
@@ -21,33 +22,47 @@ object WorkbenchPlugin extends AutoPlugin {
       "workbench-version",
       "The version of the workbench to be added as a library dependency."
     )
+    val generateWorkbenchSpec = TaskKey[Seq[File]](
+      "generate-workbench-spec",
+      "Generates a ScalaTest spec that discovers and tests schemas."
+    )
   }
   object autoImport extends Keys
   import autoImport._
 
   override lazy val projectSettings = Seq(
+    libraryDependencies += "ch.epfl.bluebrain.nexus" %% "nexus-workbench" % workbenchVersion.value % Test,
     baseUri := "http://localhost/v0",
     baseUriToken := "{{base}}",
-    libraryDependencies += "ch.epfl.bluebrain.nexus" %% "nexus-workbench" % workbenchVersion.value % Test,
-    sourceGenerators in Test += {
-      Def.task {
-        val targetDir = (sourceManaged in Test).value
-        val file      = targetDir / "ch" / "epfl" / "bluebrain" / "nexus" / "workbench" / "WorkbenchSpec.scala"
-        val content =
-          s"""
-             |package ch.epfl.bluebrain.nexus.workbench
-             |
-             |import ch.epfl.bluebrain.nexus.workbench.WorkbenchSpecLike
-             |
-             |class WorkbenchSpec extends WorkbenchSpecLike {
-             |  override def baseUri: String = "${baseUri.value}"
-             |  override def baseUriToken: String = "${baseUriToken.value}"
-             |  override def baseDir: String = "${(resourceDirectory in Compile).value}"
-             |}
-           """.stripMargin
-        IO.write(file, content)
-        Seq(file)
-      }.taskValue
-    }
+    generateWorkbenchSpec := generateSpec(
+      (sourceManaged in Test).value,
+      baseUri.value,
+      baseUriToken.value,
+      (resourceDirectory in Compile).value,
+      (resourceDirectory in Test).value
+    ),
+    sourceGenerators in Test += generateWorkbenchSpec.taskValue
   )
+
+  private def generateSpec(target: File,
+                           baseUri: String,
+                           baseUriToken: String,
+                           baseDir: File,
+                           testDir: File): Seq[File] = {
+    val file = target / "ch" / "epfl" / "bluebrain" / "nexus" / "workbench" / "WorkbenchSpec.scala"
+    if (file.exists()) Nil
+    else {
+      val content =
+        s"""|package ch.epfl.bluebrain.nexus.workbench
+            |
+            |class WorkbenchSpec extends WorkbenchSpecLike {
+            |  override def baseUri: String = "$baseUri"
+            |  override def baseUriToken: String = "$baseUriToken"
+            |  override def baseDir: String = "${baseDir.getAbsolutePath}"
+            |  override def testDir: String = "${testDir.getAbsolutePath}"
+            |}""".stripMargin
+      IO.write(file, content)
+      Seq(file)
+    }
+  }
 }
