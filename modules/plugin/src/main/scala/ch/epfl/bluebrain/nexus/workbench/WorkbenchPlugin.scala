3f1f8fe42234439eabfdf02c20e761e991e8ebe3
Bogdan Roman <bogdanromanx@gmail.com>
2017-10-20 18:16:56 +0200

    Implemented schema export as a plugin task. (#8)
    Fixes #5.

--- modules/plugin/src/main/scala/ch/epfl/bluebrain/nexus/workbench/WorkbenchPlugin.scala
+++ modules/plugin/src/main/scala/ch/epfl/bluebrain/nexus/workbench/WorkbenchPlugin.scala
@@ -3,6 +3,7 @@ package ch.epfl.bluebrain.nexus.workbench
 import sbt.Keys._
 import sbt._
 import sbt.plugins.JvmPlugin
+import sbt.complete.DefaultParsers._
 
 object WorkbenchPlugin extends AutoPlugin {
 
@@ -26,6 +27,15 @@ object WorkbenchPlugin extends AutoPlugin {
       "generate-workbench-spec",
       "Generates a ScalaTest spec that discovers and tests schemas."
     )
+    val targetExportSchemas = SettingKey[File](
+      "target-export-schemas",
+      "The default location where schemas are exported."
+    )
+    val exportSchemas = InputKey[Unit](
+      "export-schemas",
+      "Exports all schemas in the directory identified by 'target-export-schemas'; " +
+        "the task accepts the 'baseUri' and 'exportTarget' location optional positional arguments."
+    )
   }
   object autoImport extends Keys
   import autoImport._
@@ -41,20 +51,45 @@ object WorkbenchPlugin extends AutoPlugin {
       (resourceDirectory in Compile).value,
       (resourceDirectory in Test).value
     )(streams.value),
-    sourceGenerators in Test += generateWorkbenchSpec.taskValue
+    sourceGenerators in Test += generateWorkbenchSpec.taskValue,
+    targetExportSchemas := target.value / "exported",
+    exportSchemas := {
+      Def.inputTaskDyn {
+        val args = spaceDelimited("<arg>")
+          .examples(
+            "exportSchemas <baseUri> <export_target>",
+            "exportSchemas http://localhost:8080/v0 /tmp/my-schemas"
+          )
+          .parsed
+          .toList
+        val completeArgs = args match {
+          case Nil =>
+            List(baseUri.value,
+                 baseUriToken.value,
+                 (resourceDirectory in Compile).value.getAbsolutePath,
+                 targetExportSchemas.value.getAbsolutePath)
+          case first :: Nil =>
+            List(first,
+                 baseUriToken.value,
+                 (resourceDirectory in Compile).value.getAbsolutePath,
+                 targetExportSchemas.value.getAbsolutePath)
+          case first :: second :: _ =>
+            List(first, baseUriToken.value, (resourceDirectory in Compile).value.getAbsolutePath, second)
+        }
+        val arguments = completeArgs.mkString(" ")
+        streams.value.log.info(s"Running export-schemas with '$arguments'")
+        runTask(Test, "ch.epfl.bluebrain.nexus.workbench.SchemaExport", completeArgs: _*)
+      }.evaluated
+    }
   )
 
-  private def generateSpec(target: File,
-                           baseUri: String,
-                           baseUriToken: String,
-                           baseDir: File,
-                           testDir: File)(streams: TaskStreams): Seq[File] = {
+  private def generateSpec(target: File, baseUri: String, baseUriToken: String, baseDir: File, testDir: File)(
+      streams: TaskStreams): Seq[File] = {
     val file = target / "ch" / "epfl" / "bluebrain" / "nexus" / "workbench" / "WorkbenchSpec.scala"
     if (file.exists()) {
       streams.log.info(s"Workbench spec '${file.getAbsolutePath}' already exists, skipping generation.")
       Seq(file)
-    }
-    else {
+    } else {
       val content =
         s"""|package ch.epfl.bluebrain.nexus.workbench
             |
---
+++ modules/workbench/src/main/scala/ch/epfl/bluebrain/nexus/workbench/SchemaExport.scala
@@ -0,0 +1,61 @@
+package ch.epfl.bluebrain.nexus.workbench
+
+import java.io.File
+
+import akka.http.scaladsl.model.Uri
+import sbt.io._
+import sbt.io.syntax._
+import cats.instances.try_._
+
+import scala.util.control.NonFatal
+import scala.util.{Failure, Success, Try}
+
+/**
+  * Exports the collection of schemas in a configured output folder, by recursively processing the contexts such that
+  * each exported schema has a self contained context.
+  */
+object SchemaExport {
+
+  def main(args: Array[String]): Unit = {
+    val list = args.toList
+    list match {
+      case baseUri :: baseUriToken :: baseDir :: exportDir :: Nil =>
+        apply(baseUri, baseUriToken, baseDir, exportDir)
+      case _ =>
+        val msg = s"Illegal arguments: required '<baseUri> <baseUriToken>, <baseDir>, <exportDir>', got '$list'"
+        throw new IllegalArgumentException(msg)
+    }
+  }
+
+  def apply(baseUri: String, baseUriToken: String, baseDir: String, exportDir: String): Unit = {
+    val bd     = new File(baseDir).getAbsoluteFile
+    val ed     = new File(exportDir).getAbsoluteFile
+    val base   = Uri(baseUri)
+    val loader = new ResourceLoader[Try](Uri(baseUri), Map(baseUriToken -> baseUri))
+
+    println(s"Export base uri: $base")
+    println(s"Export location: ${ed.getAbsolutePath}")
+
+    schemas(bd, base)
+      .map(ref => (ref, loader(ref.uri)))
+      .foreach {
+        case (ref, Success(json)) =>
+          IO.write(new File(s"$ed/${ref.stripped}.json"), json.spaces2)
+          println(s"Exported '${ref.stripped}'")
+        case (ref, Failure(NonFatal(th))) =>
+          throw new RuntimeException(s"Failed to export schema '${ref.stripped}'", th)
+      }
+  }
+
+  private def schemas(bd: File, base: Uri): List[SchemaRef] = {
+    val finder = bd * "schemas" * "*" * "*" * "*" * "*.json"
+    finder.get.foldLeft(List.empty[SchemaRef]) {
+      case (acc, e) =>
+        val string = e.getAbsolutePath.substring(bd.getAbsolutePath.length + 1, e.getAbsolutePath.length - 5)
+        val uri    = Uri(s"$base/$string")
+        val ref    = SchemaRef(base, uri)
+        ref :: acc
+    }
+  }
+
+}
